46ec105dce79c6cf3e9043a7c189078c590d3b40
Julius Karsbrink <Julius.karsbrink@student.kyh.se>
2020-09-24 14:14:08 +0200

    så långt som jag har kommit

---
+++ src/com/company/lab3/Patient.java
@@ -0,0 +1,35 @@
+package com.company.lab3;
+
+public class Patient {
+
+    private String name;
+    private String sickness;
+    private boolean isSick;
+
+    Patient(String name, String sickness) {
+        getName();
+        isSick = true;
+
+    }
+    Patient(String name) {
+        getName();
+
+    }
+    public String getName(Patient) {
+    return name;
+    }
+    public String getSickness(String sickness){
+        return sickness;
+    }
+
+    public boolean isSick(Patient){
+        return isSick;
+        }
+
+
+
+
+    //issick getname getsickness takemedicin medlemsmetod
+
+    }
+}
